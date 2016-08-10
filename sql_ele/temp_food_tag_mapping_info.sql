7ad5082651d4a1687cee2b159e9b8a79ec3c1077
TakiyaHideto <hideto.jiahao.dong@gmail.com>
2016-08-10 20:29:25 +0800

    commit

--- sql_ele/temp_food_tag_mapping_info.sql
+++ sql_ele/temp_food_tag_mapping_info.sql
@@ -12,16 +12,27 @@ LOAD DATA LOCAL inpath '/home/jiahao.dong/file_data/food_attr.txt' OVERWRITE INT
 LOAD DATA LOCAL inpath '/home/jiahao.dong/file_data/scene_tag.txt' OVERWRITE INTO TABLE  temp.temp_food_tag_mapping_info PARTITION (part='scene_tag_origin');
 LOAD DATA LOCAL inpath '/home/jiahao.dong/file_data/function_tag.txt' OVERWRITE INTO TABLE  temp.temp_food_tag_mapping_info PARTITION (part='function_tag_origin');
 
+-- INSERT OVERWRITE TABLE temp.temp_food_tag_mapping_info PARTITION(part='normal_food_tag')
+-- SELECT t1.name, t2.value
+-- FROM ( 
+-- SELECT t.name, single_seg_food_name
+-- FROM(SELECT name, segmenter(name,true,'food_name.dic','stop.dic') AS seg_food_name FROM dw.dw_prd_food WHERE dt>'2016-07-20' AND name!='未知') t
+-- LATERAL VIEW EXPLODE(seg_food_name) myTable AS single_seg_food_name
+-- ) t1
+-- JOIN (SELECT key, value FROM temp.temp_food_tag_mapping_info WHERE part='food_attr_origin') t2
+-- ON t1.single_seg_food_name=t2.key
+-- GROUP BY t1.name, t2.value;
+
 INSERT OVERWRITE TABLE temp.temp_food_tag_mapping_info PARTITION(part='normal_food_tag')
-SELECT t1.name, t2.value
+SELECT t1.normalize_food_name, t2.value
 FROM ( 
-SELECT t.name, single_seg_food_name
-FROM(SELECT name, segmenter(name,true,'food_name.dic','stop.dic') AS seg_food_name FROM dw.dw_prd_food WHERE dt>'2016-07-20' AND name!='未知') t
+SELECT t.normalize_food_name, single_seg_food_name
+FROM(SELECT normalize_food_name, segmenter(normalize_food_name,true,'food_name.dic','stop.dic') AS seg_food_name FROM dm.dm_mdl_food_name_normalize_day WHERE dt='2016-08-08') t
 LATERAL VIEW EXPLODE(seg_food_name) myTable AS single_seg_food_name
 ) t1
 JOIN (SELECT key, value FROM temp.temp_food_tag_mapping_info WHERE part='food_attr_origin') t2
 ON t1.single_seg_food_name=t2.key
-GROUP BY t1.name, t2.value;
+GROUP BY t1.normalize_food_name, t2.value;
 
 INSERT OVERWRITE TABLE temp.temp_food_tag_mapping_info PARTITION(part='food_function')
 SELECT t2.key, t1.key
